be68edb518972ffa8737f472c82ee9d661edad2e
Haorui Zhang <1259203802@qq.com>
2024-04-14 17:57:44 +0800

    docs init v0.02

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-# HaOS
+# HaOS 设计文档
 
 HaOS, an Operating System.
 
-正在迭代开发中，不久后发布 v0.1 版本。
-
-本项目采用 MIT 协议。
-
-开发者：
-
-- [Haorui Zhang](https://github.com/Haohahahaha)
-
-欢迎有兴趣的朋友探讨与开发！
+文档主页: https://haos.haohaha.cn
--- auto-push.sh
+++ auto-push.sh
@@ -1,4 +1,4 @@
 currentdate=$(date +%Y-%m-%d_%H:%M:%S)
 git add .
-git commit -m $currentdate" update by Haorui Zhang"
-git push haos master --force
+git commit -m $currentdate" update Docs by Haorui Zhang"
+git push haos docs:docs 
